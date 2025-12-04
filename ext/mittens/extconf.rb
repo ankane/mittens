688d6304ece0d550e0740774bf2d16f709f23744
Andrew Kane <andrew@ankane.org>
2025-12-03 18:19:14 -0800

    Switched to $make

--- ext/mittens/extconf.rb
+++ ext/mittens/extconf.rb
@@ -4,7 +4,7 @@ require "open3"
 vendor = File.expand_path("../../vendor/snowball", __dir__)
 # CFLAGS from vendor/snowball/GNUmakefile and -fPIC
 cflags = "-O2 -W -Wall -Wmissing-prototypes -Wmissing-declarations -fPIC"
-output, status = Open3.capture2("make", "CFLAGS=#{cflags}", chdir: vendor)
+output, status = Open3.capture2({"CFLAGS" => cflags}, $make, chdir: vendor)
 puts output
 raise "Command failed" unless status.success?
 
