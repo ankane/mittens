81fe66500af573b9445006392add7d5ef6fe31be
Andrew Kane <andrew@ankane.org>
2025-12-07 10:06:24 -0800

    Simplified make logic - #5

--- ext/mittens/extconf.rb
+++ ext/mittens/extconf.rb
@@ -5,12 +5,7 @@ vendor = File.expand_path("../../vendor/snowball", __dir__)
 # CFLAGS from vendor/snowball/GNUmakefile and -fPIC
 cflags = "-O2 -W -Wall -Wmissing-prototypes -Wmissing-declarations -fPIC"
 
-make_cmd = "make"
-if RbConfig::CONFIG["host_os"] =~ /bsd|dragonfly/i
-  gmake = find_executable("gmake")
-  make_cmd = gmake if gmake
-end
-
+make_cmd = RbConfig::CONFIG["host_os"] =~ /bsd|dragonfly/i ? "gmake" : "make"
 output, status = Open3.capture2(make_cmd, "CFLAGS=#{cflags}", chdir: vendor)
 puts output
 raise "Command failed" unless status.success?
